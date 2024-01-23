e497c51d2efd717e238e665884fe0588d005849a
HugoGoHe <A01640856@itesm.mx>
2024-01-22 22:10:40 -0600

    Crear atributo self.radius para clase Box

--- Lift/Box.py
+++ Lift/Box.py
@@ -20,8 +20,13 @@ class Box:
             [0.5, -0.5, -0.5],
         ]
 
+        
         self.position = position
         Box.boxes.append(self)
+        # El centro del cubo son las coordenadas de self.position, por eso no creamos un atributo self.center
+        # Como los cubos son inmobiles, los centros no se actualizan
+        self.radius = 0.866  #((0.5 - self.center[0]) ** 2 + (0.5 - self.center[1]) ** 2 + (0.5 - self.center[2]) ** 2) ** 0.5 
+
 
     def render(self):
         glPushMatrix()
